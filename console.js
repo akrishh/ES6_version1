4384f1d6acfad9d4eb4116354779eea658fc7d46
Krish <akrishh@outlook.com>
2019-07-11 15:05:12 +0530

    CONSOLE commit
    CONSOLE commit

---
+++ console.js
@@ -0,0 +1,19 @@
+let car = {
+    yearModel: this.getYearModel(),
+    make: this.getYearMake(),
+    speed: this.getYearMake(),
+
+    getYearModel: function (yearvalue) {
+        return yearvalue;
+    },
+
+    getYearMake: function (yearvalue) {
+        return yearvalue;
+    },
+
+    getYearMake: function (yearvalue) {
+        return yearvalue;
+    }
+};
+
+console.log(car);
